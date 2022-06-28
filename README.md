fa1252675639bd9a61e4015057b1def0f829356b
Gustavo Bertoli <104863061+luisgustavo7141@users.noreply.github.com>
2022-06-28 10:02:46 -0300

    commit project

---
+++ README.md
@@ -0,0 +1,27 @@
+# sensedia-project
+ 
+Abaixo instruções para utilização do projeto
+
+**Tecnologias Utilizadas:**
+
+Para elaboração da atividade de automação da API Trello as tecnologias utilizadas foram:
+
+	> Linguagem de programação Java
+	> Maven para gerenciamento de dependências 
+	> BDD (Cucumber + Gherkin)
+	> RestAssured
+	> Jackson-Databind para serialização dos arquivos JSON
+	> Junit 5 para realização das validações
+	> IDE Eclipse
+	
+**Configuração do ambiente**
+
+1. Dependências Globais
+	* Instalar o [Maven](https://maven.apache.org/install.html)
+2. Dependências do projeto
+	* Verifique se os pacotes (JUnit 5, RestAssured, Cucumber, Jackson-Databind) estão disponíveis, os mesmos já estão configurados no arquivo .pom do projeto
+
+**Conselhos/Solução de Problemas**
+
+Pode ser útil usar um IDE Java, como IntelliJ ou Eclipse, para ajudar a solucionar possíveis problemas e auxiliar na execução da ferramenta.
+
---
+++ src/test/java/objects/BoardObject.java
@@ -0,0 +1,62 @@
+package objects;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import utils.BaseTest;
+
+/**
+ * @author lbertoli
+ *
+ */
+public class BoardObject extends BaseTest {
+	
+	final String endpoint = "/1/boards";
+	private Response response;
+		
+	//REALIZA CRIAÇÃO DO CARD
+	public Response createBoard(String name){
+		response =				
+				given()
+					.header("Content-type", "application/json")
+                .when()
+                	.post(endpoint + "?name=" + name + "&key=" + CHAVE_API + "&token=" + TOKEN)
+                .then()
+                	.log().all()
+                	.extract().response();	
+		
+			System.out.println("Foi realizado uma criação do board: " + response.asString());
+			return response;
+	}
+
+	//REALIZA REMOÇÃO DO CARD
+	public Response removeBoard(String idBoard) {
+		response =	
+				given()
+					.header("Content-type", "application/json")
+	                .when()
+	                	.delete(endpoint + "/" + idBoard + "?key=" + CHAVE_API + "&token=" + TOKEN)
+	                .then()
+	                	.log().all()
+	                	.extract().response();
+		
+	    System.out.println("O board: " + idBoard + " foi removido com sucesso!");		
+	    return response;
+	}
+	
+	public String getIdList(String idBoard) {
+
+		Response response =				
+				given()
+					.contentType(ContentType.JSON)
+			   .when()
+	               	  .get(endpoint + "/" + idBoard + "/lists?key=" + CHAVE_API + "&token=" + TOKEN)
+			   .then()
+	              	.log().all()
+	              	.extract().response();
+		return response.jsonPath().getString("[0].id");
+
+	}
+		
+}
